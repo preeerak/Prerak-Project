9b0cd014770a580fcfcff2bd01d835fee2582f81
preeerak <190303105295@paruluniversity.ac.in>
2022-11-24 16:22:16 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { HomeComponent } from './pages/home/home.component';
+import { PreparingComponent } from './pages/preparing/preparing.component';
+import { SourcingComponent } from './pages/sourcing/sourcing.component';
+import { ScreeningComponent } from './pages/screening/screening.component';
+import { SelectingComponent } from './pages/selecting/selecting.component';
+import { HiringComponent } from './pages/hiring/hiring.component';
+import { OnboardingComponent } from './pages/onboarding/onboarding.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './pages/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PreparingComponent,
+    SourcingComponent,
+    ScreeningComponent,
+    SelectingComponent,
+    HiringComponent,
+    OnboardingComponent,
+    NotFoundComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/preparing/preparing.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-preparing',
+  templateUrl: './preparing.component.html',
+  styleUrls: ['./preparing.component.scss']
+})
+
+export class PreparingComponent{
+
+  constructor() {} 
+  
+}
